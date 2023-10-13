e70db6609b8143ebb2833537855f2c668df0b645
Alexandra <bor-18@mail.ru>
2023-10-13 17:30:41 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,13 @@
+import { lazy } from "react";
+
+// Лучше использовать асинхронный импорт в чанках (т.е разбивка бандл на чанки)
+// const AboutPageAsync = lazy(() => import("./AboutPage"));
+
+export const AboutPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      // @ts-ignore
+      // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+      setTimeout(() => resolve(import("./AboutPage")), 1500);
+    })
+);
