29d72e06f16f93d45a50d0a9a8eeb4acdeab7b15
Alexandra <bor-18@mail.ru>
2023-10-19 20:38:30 +0300

    add eslint, fix all files

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,13 +1,12 @@
-import { lazy } from "react";
+import { lazy } from 'react';
 
 // Лучше использовать асинхронный импорт в чанках (т.е разбивка бандл на чанки)
 // const AboutPageAsync = lazy(() => import("./AboutPage"));
 
 export const AboutPageAsync = lazy(
-  () =>
-    new Promise((resolve) => {
-      // @ts-ignore
-      // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-      setTimeout(() => resolve(import("./AboutPage")), 1500);
-    })
+    () => new Promise((resolve) => {
+        // @ts-ignore
+        // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+        setTimeout(() => resolve(import('./AboutPage')), 1500);
+    }),
 );
