62cc1496fb3e06f71fa447e14e405d8d26a47c0e
Alexandra <bor-18@mail.ru>
2024-01-29 16:11:36 +0300

    add modal, portal, change header

--- README.md
+++ README.md
@@ -7,25 +7,25 @@
 - Интернационализация ( i18next )
 - Изменение темы с помощью контекста React
 - Тестовая среда ( Storybook, jest )
-  
+
 #### Используемые технологии:
 
 - React
 - Redux
-- Typescript
+- TypeScript
 - JavaScript
 - SCSS
 - HTML5/CSS3
 
 #### Статус:
 
-В разработке. 
+В разработке.
 
 #### Запуск проекта:
 
 ```
 git clone https://github.com/8Alex/production-project.git
 cd production-project
-npm install  
+npm install
 npm start
 ```
---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+const meta: Meta<typeof Modal> = {
+    title: 'shared/Modal',
+    component: Modal,
+    // argTypes: {
+    //     backgroundColor: { control: 'color' },
+    // },
+};
+export default meta;
+
+type Story = StoryObj<typeof Modal>;
+
+export const Primary: Story = {
+    args: {
+        isOpen: true,
+        children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
+    },
+};
+
+export const Dark: Story = {
+    args: {
+        isOpen: true,
+        children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
+    },
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
